86d35e554721b8de0f8b7e06f92b89bf6df2f79c
SunstriderEx <sunstrider101@mail.ru>
2014-12-27 16:09:17 +0500

    1st
    description

---
+++ Assets/Scripts/BallBehavior.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallBehavior : MonoBehaviour {
+    Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        //gameObject.transform.position = new Vector3(0, 0);
+        animator = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButton(0))
+        {
+            gameObject.rigidbody2D.AddForce((mousePos - gameObject.transform.position) * 10f);
+        }
+
+        animator.SetFloat("Speed", gameObject.rigidbody2D.velocity.magnitude);
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Bounds")
+        {
+            animator.SetTrigger("Die");
+        }
+    }
+
+    public void DieEvent()
+    {
+        DestroyObject(gameObject);
+    }
+}
