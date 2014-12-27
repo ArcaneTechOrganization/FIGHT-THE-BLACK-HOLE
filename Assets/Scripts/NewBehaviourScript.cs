0367053f2db5406560e2aece9623ee52e196092d
TheDVolk007 <dvolk007@mail.ru>
2014-12-27 14:40:23 +0300

    new ball

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButton(0))
+        {
+            gameObject.rigidbody2D.AddForce((mousePos - gameObject.transform.position) * 1f);
+        }
+
+       
+	}
+}
